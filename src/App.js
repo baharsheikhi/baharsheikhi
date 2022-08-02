edacc75a7cb76cd5ae6a4b8637618c7d17e37c09
Bahar Sheikhi <baharsheikhi@Bahars-MacBook-Pro.local>
2022-08-02 18:07:52 -0400

    some refactoring

--- src/App.js
+++ src/App.js
@@ -26,34 +26,38 @@ function App() {
 
   return (
     <div className="App">
+      {/* Introduction section  */}
       <div className="App-Header">
         <div className="App-Header-Content">
-          <BounceText shake={true} childClassName="App-Header-Content-Title">
+          <BounceText shake={true} childClassName="App-Title">
             Introduction
           </BounceText>
           <div>
-            <p className="App-Header-Content-Introduction">
+            <p className="App-Text">
               {introduction}
             </p>
           </div>
-          <BounceText shake={shake} childClassName="App-Header-Content-Introduction">
+          <BounceText shake={shake} childClassName="App-Text">
             {count}
           </BounceText>
-          <div className="App-Header-Content-Introduction-Button">
-            <button onClick={myfunct}> Surprise me</button>
+          <div className="App-Header-Content-Introduction-Button-Container">
+            <button className="App-Header-Content-Introduction-Button" onClick={myfunct}> Surprise me</button>
           </div>
         </div>
       </div>
+
+
+      {/* Experience section  */}
       <div className="App-Experience">
-        <p className="App-Header-Content-Title">
+        <p className="App-Title">
           Experience
         </p>
         <div className="App-Experience-Content-Container">
           <div className="App-Experience-Content-Container-Column-Left">
-            <p className={classNames("App-Header-Content-Introduction", "App-Header-Experience-Content-Text")}><b>Software Engineer at Alma</b><br /> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p className={classNames("App-Text", "App-Header-Experience-Content-Text")}><b>Software Engineer at Alma</b><br /> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
           <div className="App-Experience-Content-Container-Column-Right">
-            <p className={classNames("App-Header-Content-Introduction", "App-Header-Experience-Content-Text")}><b>Software Engineer at Drift</b><br /> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p className={classNames("App-Text", "App-Header-Experience-Content-Text")}><b>Software Engineer at Drift</b><br /> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
         </div>
       </div>
